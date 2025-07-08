d746fe85233cad32151654dee6bc6661df76a844
israrkhann08 <israrkhann08@gmail.com>
2025-07-09 04:35:36 +0500

    updates in recognizer listing show continously and also recognizer we stop this

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ def command():
     r = sr.Recognizer()
     with sr.Microphone() as source:
         r.adjust_for_ambient_noise(source, duration=0.5)
-        print("Listening......")
+        print("Listening......", end="", flush=True)
         r.pause_threshold=1.0 # properties for the recognizer
         r.phrase_threshold=0.3
         r.sample_rate = 48000
@@ -33,8 +33,10 @@ def command():
         # print(sr.Microphone.list_microphones_names())
         audio = r.listen(source)
     try:
-        print('Recognizing.....')
+        print("\r", end="", flush=True)
+        print("Recognizing.....", end="", flush=True)
         query = r.recognize_google(audio, language= 'en-in')
+        print("\r", end="", flush=True)
         print(f"User Said: {query}\n")
     except Exception as e:
         print('Say, that again please')
